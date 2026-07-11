13e905d3b58475e3cae720fb26c8c9f9532cb7e5
Vashu2707 <github@vashu.in>
2026-07-11 12:03:34 +0530

    added new proj

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -10,11 +10,20 @@ export interface Project {
 
 export const projects: Project[] = [
   {
+    id: '0',
+    title: 'QRFast',
+    description:
+      'Full-stack web application for restaurant management, including menu display, order tracking, and payment processing using Next.js, Supabase and Razorpay.',
+    technologies: ['Next.js', 'Node.js', 'Supabase', 'Tailwind CSS', 'Razorpay'],
+    link: 'https://qrfast.vashu.in/',
+    github: 'https://github.com/Vashu2707/qrfast',
+  },
+  {
     id: '1',
     title: 'PayNGo Retail',
     description:
-      'Full-stack web application to manage student records, attendance, and grading using React, Node.js, MongoDB, and Tailwind CSS.',
-    technologies: ['React', 'Node.js', 'MongoDB', 'Tailwind CSS'],
+      'Full-stack web application to manage student records, attendance, and grading using Next.js, Node.js, MongoDB, and Tailwind CSS.',
+    technologies: ['Next.js', 'Node.js', 'MongoDB', 'Tailwind CSS'],
     link: 'https://github.com/Vashu2707/PayNGo',
     github: 'https://github.com/Vashu2707/PayNGo',
   },
@@ -22,8 +31,8 @@ export const projects: Project[] = [
     id: '2',
     title: 'Eduque',
     description:
-      'Classic Othello/Reversi game using JavaScript and HTML Canvas with dynamic game logic and responsive UI.',
-    technologies: ['JavaScript', 'HTML Canvas', 'CSS'],
+      'A full fledged application for schools and educational institutions to manage student records, attendance, and grading using Next.js, Node.js, MongoDB, and Tailwind CSS.',
+    technologies: ['Next.js', 'Node.js', 'Supabase', 'Tailwind CSS'],
     link: 'https://eduque.vashu.in/',
     github: 'https://github.com/Vashu2707/Eduque',
   },
@@ -31,8 +40,8 @@ export const projects: Project[] = [
     id: '3',
     title: 'Eventics',
     description:
-      'Minimal task management app with React and local storage, featuring CRUD operations and real-time updates.',
-    technologies: ['React', 'JavaScript', 'Local Storage'],
+      'A web application for event management, allowing users to create, manage, and track events with features like ticketing, scheduling, and attendee management.',
+    technologies: ['Next.js', 'Node.js', 'Supabase', 'Tailwind CSS'],
     link: 'https://eventics.vashu.in/',
     github: 'https://github.com/Vashu2707/Eventics',
   },
@@ -40,7 +49,7 @@ export const projects: Project[] = [
     id: '4',
     title: 'Academic Management',
     description:
-      'Interactive game with real-time score tracking, animations, and intuitive UI/UX.',
+      'A web application for managing attendance.',
     technologies: ['HTML', 'CSS', 'JavaScript'],
     link: 'https://academic-management.vashu.in/',
     github: 'https://github.com/Vashu2707/Academic-Management',
